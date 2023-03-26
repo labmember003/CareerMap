111eaa892d61234f3a2ad9aa99325e7974a892da
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-03-26 22:23:21 +0530

    reduced to 3 questions

--- app/src/main/java/com/falcon/careermap/FirstFragment.kt
+++ app/src/main/java/com/falcon/careermap/FirstFragment.kt
@@ -29,4 +29,27 @@ class FirstFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+    private fun updateValues() {
+        // QUESTIONS
+        binding.question1Textview.text
+        binding.question2Textview.text
+        binding.question3Textview.text
+        binding.question4Textview.text
+
+        //Q1 ANSWERS
+        binding.answer11Radiobutton.text
+        binding.answer12Radiobutton.text
+        binding.answer13Radiobutton.text
+
+        //Q2 ANSWERS
+        binding.answer21Radiobutton.text
+        binding.answer22Radiobutton.text
+        binding.answer23Radiobutton.text
+
+        //Q3 ANSWERS
+        binding.answer31Radiobutton.text
+        binding.answer32Radiobutton.text
+        binding.answer33Radiobutton.text
+
+    }
 }
