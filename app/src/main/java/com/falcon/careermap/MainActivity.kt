50afc3c6c5cec381c4defcca8ef57367bedb996d
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-03-26 18:41:42 +0530

    added settings activity

--- app/src/main/java/com/falcon/careermap/MainActivity.kt
+++ app/src/main/java/com/falcon/careermap/MainActivity.kt
@@ -54,11 +54,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                val myIntent = Intent(this, SettingsActivity::class.java)
+                startActivity(myIntent)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
