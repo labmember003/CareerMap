f98b5a58674b8a79bf5e81c5788e80d3e83f1ca8
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-04-09 14:29:54 +0530

    fixed using dynamic adding layout

--- app/src/main/java/com/falcon/careermap/adapters/RcvQuestionnaire.kt
+++ app/src/main/java/com/falcon/careermap/adapters/RcvQuestionnaire.kt
@@ -4,14 +4,16 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.falcon.careermap.R
 
 class RcvQuestionnaire(private val questionList: ArrayList<String>, private val answerList: ArrayList<ArrayList<String>>, private val context : Context,
                        private val onItemSelect : (Int, Int, Boolean) -> Unit
                        ) : RecyclerView.Adapter<RcvQuestionnaire.QuestionViewHolder>() {
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.question_answer, parent, false)
@@ -21,8 +23,11 @@ class RcvQuestionnaire(private val questionList: ArrayList<String>, private val 
     override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) {
         holder.question.text = questionList[position]
         val listOfAnswer = answerList[position]
-        holder.answerList.adapter = AnswerAdapter(listOfAnswer,::onItemSelected, position)
-        holder.answerList.layoutManager = LinearLayoutManager(context)
+        for (i in 1..listOfAnswer.size) {
+            val answerView = LayoutInflater.from(holder.itemView.context).inflate(R.layout.answer_single, holder.itemView as ViewGroup, false)
+            val parentLayout = holder.holderLayout
+            parentLayout.addView(answerView)
+        }
     }
     private fun onItemSelected(questionNumber: Int, answerIndex: Int, isSelected: Boolean) {
         onItemSelect(questionNumber, answerIndex, isSelected)
@@ -32,6 +37,6 @@ class RcvQuestionnaire(private val questionList: ArrayList<String>, private val 
     }
     class QuestionViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val question: TextView = itemView.findViewById(R.id.question_textview)
-        val answerList: RecyclerView = itemView.findViewById(R.id.answerRecyclerView)
+        val holderLayout: LinearLayout = itemView.findViewById(R.id.question_LL)
     }
 }
